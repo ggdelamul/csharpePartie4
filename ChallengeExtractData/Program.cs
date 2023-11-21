561b9320696e528c064105b7e67a877d56864800
jeremy <projeremylegendre@gmail.com>
2023-11-21 18:44:47 +0100

    added remove and replace method

---
+++ ChallengeExtractData/Program.cs
@@ -0,0 +1,31 @@
+﻿const string input = 
+"<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+string quantity = "";
+string output = "";
+// Your work here
+//1 Affectez à la variable quantity la valeur en extrayant le texte entre les balises <span> et </span>
+const string openSpan = "<span>";
+const string closeSpan = "</span>";
+int openingPosition = input.IndexOf(openSpan);
+int closingPosition = input.IndexOf(closeSpan);
+Console.WriteLine(openingPosition);
+int length = closingPosition-openingPosition;
+openingPosition +=6;
+quantity = input.Substring(openingPosition, length-6);
+//2 Affectez à la variable output la valeur d’entrée, puis supprimez les balises <div> et </div>.
+//3Remplacez le caractère HTML ™ (&trade;) par dans la variable ® (&reg) dans la variable output.
+const string closeDiv = "</div>";
+int closingPositionDiv = input.IndexOf(closeDiv);
+// Console.WriteLine($"{closingPositionDiv} ICI tag fermeture div ");
+// Console.WriteLine(input[46]);
+output = input.Remove(0,4);
+output=input.Remove(closingPositionDiv, 6);
+output= input.Replace("&trade;","&reg;");
+// Console.WriteLine(output);
+/* ouput
+Quantity: 5000
+Output: <h2>Widgets &reg;</h2><span>5000</span>
+*/
+Console.WriteLine($"Quantity: {quantity}");
+Console.WriteLine($"Output: {output}");
+
--- RemoveReplace/Program.cs
+++ RemoveReplace/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿///methode remove
+//Supprimer des caractères dans des emplacements spécifiques d’une chaîne
+string data = "12345John Smith          5000  3  ";
+string updatedData = data.Remove(5, 20);
+//position de depart , nbr de caractère
+Console.WriteLine(updatedData);
+
+///methode replace
+///Supprimer des caractères, où qu’ils soient dans une chaîne
+///
+string message = "This--is--ex-amp-le--da-ta";
+message = message.Replace("--", " ");
+message = message.Replace("-", "");
+Console.WriteLine(message);
